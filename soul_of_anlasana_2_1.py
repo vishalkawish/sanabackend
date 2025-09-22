c351928c8a70195ff7565e4f964b03a0ca2a5077
vishalkawish <61213868+vishalkawish@users.noreply.github.com>
2025-09-22 00:50:51 +0000

    update

--- soul_of_anlasana_2_1.py
+++ soul_of_anlasana_2_1.py
@@ -222,7 +222,7 @@ def soul_of_anlasana(user_id: str):
             "url": other.get("profile_pic_url"),
             "type": ctype,
             "match_percent": score,
-            "age": other.get("age")  # include age in response
+            "age": other.get("age"),  # include age in response
             "number": other.get("phone_number")  # include phone number in response
         })
 
